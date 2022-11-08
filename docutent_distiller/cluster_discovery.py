b9b0f1ea4e1099e3b7d9c5e6e15078385d28753f
csanyig <csanyi.gergely@montana.hu>
2022-11-08 15:26:17 +0100

    cluster discovery improved

--- docutent_distiller/cluster_discovery.py
+++ docutent_distiller/cluster_discovery.py
@@ -1,4 +1,4 @@
-from distiller.data_snapshot import DataSnapshot
+from data_snapshot import DataSnapshot
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 from importlib_resources import files
@@ -7,8 +7,8 @@ import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
-import pyLDAvis
-import pyLDAvis.sklearn
+# import pyLDAvis
+# import pyLDAvis.sklearn
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import pandas as pd
@@ -16,11 +16,131 @@ import re
 import seaborn as sns
 
 
-class RelevantExpressionFinder:
-    def __init__(self, X):
+class ClusterDiscovery:
+    def __init__(self, X, feature_count=1000, n_top_words=50, random_state=1):
         """
-        :param X: List of documents
+        :param X: Data points in list of strings form.
         """
+        self.X = X
+        self.random_state = random_state
+        self.feature_count = feature_count
+        self.n_top_words = n_top_words
+        self.create_vectorizer(self.X)
+
+    def create_vectorizer(self, **vectorizer_settings):
+        tfidf_vectorizer = TfidfVectorizer(**vectorizer_settings)
+        tfidf_vectorizer = tfidf_vectorizer.fit(self.X)
+        self.tfidf_vectorizer = tfidf_vectorizer
+
+    def get_top_features_cluster(self, tf_idf_array, prediction, n_feats, tf_idf_vectorizer):
+        labels = np.unique(prediction)
+        dfs = []
+        best_features_list = []
+        for label in labels:
+            id_temp = np.where(prediction == label)  # indices for each cluster
+            x_means = np.mean(tf_idf_array[id_temp], axis=0)  # returns average score across cluster
+            sorted_means = np.argsort(x_means)[::-1][:n_feats]  # indices with top 20 scores
+            features = tf_idf_vectorizer.get_feature_names()
+            best_features = [(features[i], x_means[i]) for i in sorted_means]
+            df = pd.DataFrame(best_features, columns=['features', 'score'])
+            dfs.append(df)
+            best_features_list.append(best_features)
+        return dfs, best_features_list
+
+    def plot_entropies(self, results_list):
+        for idx, entropy_words in enumerate(results_list):
+            # reversing order
+            entropy_words = entropy_words[::-1]
+
+            words = [word[0] for word in entropy_words]
+
+            entropies = [word[1] for word in entropy_words]
+            y_pos = np.arange(len(words))
+            max_entropy = max(entropies)
+
+            entropies = [max_entropy - word[1] for word in entropy_words]
+            plt.barh(y_pos, entropies, tick_label=words)
+            plt.title("Most relevant words for Cluster Nr. {}".format(idx + 1))
+            plt.xlabel("Relevance score")
+            plt.ylabel("Words")
+            plt.show()
+
+    def plot_features(self, dfs):
+        fig = plt.figure(figsize=(14, 12))
+        x = np.arange(len(dfs[0]))
+        for i, df in enumerate(dfs):
+            ax = fig.add_subplot(1, len(dfs), i + 1)
+            ax.set_title("Cluster: " + str(i), fontsize=14)
+            ax.spines["top"].set_visible(False)
+            ax.spines["right"].set_visible(False)
+            ax.set_frame_on(False)
+            ax.get_xaxis().tick_bottom()
+            ax.get_yaxis().tick_left()
+            ax.ticklabel_format(axis='x', style='sci', scilimits=(-2, 2))
+            ax.barh(x, df.score, align='center', color='#40826d')
+            yticks = ax.set_yticklabels(df.features)
+        plt.show();
+
+    def plot_sns(self, dfs, nr_features):
+        for i in range(len(dfs)):
+            plt.figure(figsize=(8, 6))
+            sns.barplot(x='score', y='features', orient='h', data=dfs[i][:nr_features])
+
+    def plot_clusters(self, kmeans, Y_sklearn, use_colors=False):
+        if use_colors:
+            predicted_values = kmeans.predict(Y_sklearn)
+
+            plt.scatter(Y_sklearn[:, 0], Y_sklearn[:, 1], c=predicted_values, s=50, cmap='viridis')
+
+            centers = kmeans.cluster_centers_
+            plt.scatter(centers[:, 0], centers[:, 1], c='black', s=300, alpha=0.6)
+        else:
+            plt.scatter(Y_sklearn[:, 0], Y_sklearn[:, 1], s=50)
+        plt.show()
+
+    def get_entropy_based_best_candidates(self, results_list):
+        topic_union_set = set([elem[0] for res in results_list for elem in res])
+        topic_sets = []
+        for res in results_list:
+            topic_sets.append(set([elem[0] for elem in res]))
+        res_dict = {}
+        res_lst = []
+        for elem in topic_union_set:
+            res_dict[elem] = []
+            for res in results_list:
+                if not elem in [wordscore[0] for wordscore in res]:
+                    res_dict[elem].append(0)
+                else:
+                    for word, score in res:
+                        if word == elem:
+                            res_dict[elem].append(score)
+        for topic_set in topic_sets:
+            topic_entropy = [(word, entropy(res_dict.get(word))) for word in topic_set]
+            topic_entropy = sorted(topic_entropy, key=lambda tup: tup[1])
+            res_lst.append(topic_entropy)
+        return res_lst
+
+    def fit(self, X, number_of_topics=10):
+        inertias = []
+        print("Number of clusters: {}".format(number_of_topics))
+        kmeans = KMeans(n_clusters=number_of_topics, max_iter=600, random_state=self.random_state).fit(X)
+        # PCA
+        dfs, best_features_list = get_top_features_cluster(X, kmeans.predict(Y_sklearn), n_top_words,
+                                                           tfidf_vectorizer)
+        entropy_features = get_entropy_based_best_candidates(best_features_list)
+        for elem in entropy_features:
+            print(elem)
+        plot_entropies(entropy_features)
+        #
+        plot_clusters(kmeans, Y_sklearn, use_colors=True)
+
+        # print("Inertias: {}".format(inertias))
+        # fig, (ax1) = plt.subplots(1, 1)
+        # ax1.plot(topic_nums, inertias, label="Inertia")
+        # ax1.legend()
+        # ax1.set_xlabel("Number of clusters")
+        # ax1.set_ylabel("Sum of squared distances of samples to their closest cluster center")
+        # plt.show()
 
 
 regex_filter_pat = re.compile(
@@ -45,9 +165,9 @@ X = [doc for doc in X if regex_filter_pat.search(doc)]
 
 RANDOM_STATE = 1
 FEATURE_COUNT = 1000
-topic_nums = [11]#[elem for elem in range(1, 30, 2)]
+topic_nums = [11]  # [elem for elem in range(1, 30, 2)]
 n_top_words = 50
-use_filter_reduction = False
+
 nltk_stopwords = ['után', 'ilyenkor', 'amíg', 'utolsó', 'olyan', 'maga', 'elég', 'ill.', 'milyen', 'mivel', 'akik',
                   'amit', 'ezen', 'vissza', 'ami', 'amelyek', 'lett', 'ezzel', 'sokkal', 'újra', 'össze', 'amelyekben',
                   'ebben', 'ezek', 'egyetlen', 'mint', 'saját', 'új', 'szinte', 'abban', 'mellett', 'mi', 'lehet',
@@ -108,7 +228,7 @@ def plot_entropies(results_list):
 
         entropies = [max_entropy - word[1] for word in entropy_words]
         plt.barh(y_pos, entropies, tick_label=words)
-        plt.title("Most relevant words for Cluster Nr. {}".format(idx+1))
+        plt.title("Most relevant words for Cluster Nr. {}".format(idx + 1))
         plt.xlabel("Relevance score")
         plt.ylabel("Words")
         plt.show()
