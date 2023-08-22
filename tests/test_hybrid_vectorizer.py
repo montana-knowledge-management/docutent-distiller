210f58b9a4d297ea767a3b34f79e40ca1fff8cb3
lakatosd <lakatos.dorina@montana.hu>
2023-08-22 09:49:56 +0200

    add HybridVectorizer

---
+++ tests/test_hybrid_vectorizer.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+from unittest import mock
+
+from docutent_distiller.hybrid_bert_vectorizer import HybridVectorizer
+import numpy as np
+
+
+class MyTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.vectorizer = HybridVectorizer()
+
+    def test_almost_equal_vectors(self):
+        text = "Ez egy teszt mondat."
+        short_vec = self.vectorizer.short_vectorizer.get_cls_token_embedding([text])[0]
+        long_vec = self.vectorizer.long_vectorizer.vectorize(text)
+        self.assertTrue(np.allclose(short_vec, long_vec, atol=1e-5))
+
+    def test_long_sentence(self):
+        text = "Ez egy {} hosszú mondat.".format(
+            " ".join(["igen nagyon fű de nagyon tartalmas és unalmas és hosszú"] * 100)
+        )
+        short_vec = self.vectorizer.short_vectorizer.get_cls_token_embedding([text])[0]
+        long_vec = self.vectorizer.vectorize([text])[0]
+        self.assertFalse(np.allclose(short_vec, long_vec, atol=1e-5))
+
+    def test_batching(self):
+        sentences = [
+            "Ez egy teszt mondat.",
+            "Ez egy {} hosszú mondat.".format(
+                " ".join(["igen nagyon fú de nagyon tartalmas és unalmas és hosszú"] * 100)
+            ),
+            "Ez egy másik harmadik teszt mondat.",
+        ]
+
+        with mock.patch.dict(os.environ, {"BATCH_SIZE": "2"}):
+            self.vectorizer = HybridVectorizer()
+            vectors_batch_2 = self.vectorizer.vectorize(sentences)
+
+        with mock.patch.dict(os.environ, {"BATCH_SIZE": "1"}):
+            self.vectorizer = HybridVectorizer()
+            vectors_batch_1 = self.vectorizer.vectorize(sentences)
+
+        self.assertEquals(vectors_batch_2.shape, (3, 768))
+        self.assertTrue(np.allclose(vectors_batch_1, vectors_batch_2, atol=1e-5))
+
+
+if __name__ == "__main__":
+    unittest.main()
